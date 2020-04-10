fdb91482a1643bf5e59e719ee931e9f2bf883619
Arun Mathew <arunvmathew95@gmail.com>
2020-04-10 15:14:43 +0530

    Added comments to admin check handler to provide clarity

--- handler_admin_check.go
+++ handler_admin_check.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// The middleware checks if the user is an admin. So if it reaches this handler
+// the user is an admin and can straight away send the response
 func (server *server) adminCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, struct {
 		Admin bool `json:"admin"`
