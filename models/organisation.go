3237c6d0273de92ed128d19e401eca157cc721b0
Arun Mathew <arunvmathew95@gmail.com>
2020-04-21 23:20:24 +0530

    Added fields 'theme' and 'display picture' to organisation model and updated related APIs

--- models/organisation.go
+++ models/organisation.go
@@ -11,12 +11,14 @@ import (
 
 // Organisation groups all the teams together and is typically the company name
 type Organisation struct {
-	ID        string `json:"id" gorm:"primary_key;auto_increment:false"`
-	AdminID   int    `json:"adminID"`
-	Name      string `json:"name"`
-	Type      int    `json:"type"` // Type denotes if this is the user's personal space or of a companies
-	CreatedAt int64  `json:"createdAt"`
-	Archived  bool   `json:"archived"`
+	ID             string  `json:"id" gorm:"primary_key;auto_increment:false"`
+	AdminID        int     `json:"adminID"`
+	Name           string  `json:"name"`
+	DisplayPicture *string `json:"displayPicture"`
+	Theme          string  `json:"theme"`
+	Type           int     `json:"type"` // Type denotes if this is the user's personal space or of a companies
+	CreatedAt      int64   `json:"createdAt"`
+	Archived       bool    `json:"archived"`
 }
 
 const (
@@ -36,22 +38,28 @@ func (org *Organisation) Save(db *gorm.DB) error {
 }
 
 type CreateOrganisationArgs struct {
-	Name string `json:"name" binding:"required"`
+	Name           string  `json:"name" binding:"required"`
+	DisplayPicture *string `json:"displayPicture"`
+	Theme          string  `json:"theme" binding:"required"`
 }
 
 type UpdateOrganisationArgs struct {
-	ID   string `json:"id"`
-	Name string `json:"name" binding:"required"`
+	ID             string  `json:"id"`
+	Name           *string `json:"name"`
+	DisplayPicture *string `json:"displayPicture"`
+	Theme          *string `json:"theme"`
 }
 
 func (user *User) CreateOrganisation(db *gorm.DB, args *CreateOrganisationArgs) error {
 	org := Organisation{
-		ID:        uuid.NewV4().String(),
-		AdminID:   user.ID,
-		Archived:  false,
-		CreatedAt: time.Now().Unix(),
-		Name:      args.Name,
-		Type:      organistation,
+		ID:             uuid.NewV4().String(),
+		AdminID:        user.ID,
+		Archived:       false,
+		CreatedAt:      time.Now().Unix(),
+		Name:           args.Name,
+		Type:           organistation,
+		DisplayPicture: args.DisplayPicture,
+		Theme:          args.Theme,
 	}
 
 	err := org.Create(db)
@@ -109,7 +117,16 @@ func (admin *User) UpdateOrganisation(db *gorm.DB, args *UpdateOrganisationArgs)
 		return err
 	}
 
-	org.Name = args.Name
+	if args.Name != nil {
+		org.Name = *args.Name
+	}
+	if args.DisplayPicture != nil {
+		org.DisplayPicture = args.DisplayPicture
+	}
+	if args.Theme != nil {
+		org.Theme = *args.Theme
+	}
+
 	err = org.Save(db)
 	if err != nil {
 		log.WithFields(log.Fields{
